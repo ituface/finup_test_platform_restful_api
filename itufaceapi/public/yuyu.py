6595cec823417dd7049a1da9da6cfd3b05975730
ituface <ituface@163.com>
2019-07-15 15:36:03 +0800

    修改人脸识别直接到销售质检

--- itufaceapi/public/yuyu.py
+++
@@ -1,5 +0,0 @@
-from public.mysql import MysqlHandle
-sql = MysqlHandle.get_xml_sql(xml_path='select_sql', xml_tag='select', xml_id='select_mobile_enable')
-print(sql.format(mobile='xy9482e8393b6e98ff2d8c7e0080c599a320160926'))
-tag = MysqlHandle.select_mysql_data()
-print(tag)
